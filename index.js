d4bb615c6cb9c20aa38db93a808c6af18c2c73be
sandeep-incedo <iamsandeep344@gmail.com>
2024-07-12 12:29:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, resp, next) => {
+  res.send('Hello, world!');  
+});
+
 app.listen(3000, () => {
   console.log("Server listing at port 3000");
 });
